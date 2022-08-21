96fedc4cf347219599ec5395cc232db6bdec27a0
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-08-21 14:16:00 +0100

    added Hero room entity

---
+++ app/src/main/java/com/yasserakbbach/borutoapp/domain/model/Hero.kt
@@ -0,0 +1,19 @@
+package com.yasserakbbach.borutoapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "hero_table")
+data class Hero(
+    @PrimaryKey(autoGenerate = false) val id: Int,
+    val name: String,
+    val image: String,
+    val about: String,
+    val rating: Double,
+    val power: Int,
+    val month: String,
+    val day: String,
+    val family: List<String>,
+    val abilities: List<String>,
+    val natureTypes: List<String>,
+)
