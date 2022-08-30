f7f36aea172e3cc05bf235d439bbd1379140f374
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-08-30 22:37:26 +0100

    implemented hero listing shimmer effect

--- app/src/main/java/com/yasserakbbach/borutoapp/presentation/components/hero/HeroShimmerEffect.kt
+++ app/src/main/java/com/yasserakbbach/borutoapp/presentation/components/hero/HeroShimmerEffect.kt
@@ -8,19 +8,20 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.tooling.preview.Preview
 import com.yasserakbbach.borutoapp.ui.theme.ABOUT_PLACEHOLDER_HEIGHT
 import com.yasserakbbach.borutoapp.ui.theme.EXTRA_SMALL_PADDING
 import com.yasserakbbach.borutoapp.ui.theme.HERO_ITEM_HEIGHT
@@ -34,7 +35,14 @@ import com.yasserakbbach.borutoapp.ui.theme.SMALL_PADDING
 
 @Composable
 fun HeroShimmerEffect() {
-
+    LazyColumn(
+        contentPadding = PaddingValues(all = SMALL_PADDING),
+        verticalArrangement = Arrangement.spacedBy(SMALL_PADDING),
+    ) {
+        items(2) {
+            AnimatedHeroShimmerItem()
+        }
+    }
 }
 
 @Composable
@@ -94,7 +102,8 @@ fun HeroShimmerItem(
             ) {
                 repeat(5) {
                     Surface(
-                        modifier = Modifier.alpha(alpha)
+                        modifier = Modifier
+                            .alpha(alpha)
                             .size(RATING_PLACEHOLDER_SIZE),
                         color = HeroItemNameShimmerBackgroundColor,
                         shape = RoundedCornerShape(size = EXTRA_SMALL_PADDING),
@@ -104,10 +113,4 @@ fun HeroShimmerItem(
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun ShimmerItemPreview() {
-    AnimatedHeroShimmerItem()
 }
--- app/src/main/java/com/yasserakbbach/borutoapp/presentation/screens/hero/ListHeroesContent.kt
+++ app/src/main/java/com/yasserakbbach/borutoapp/presentation/screens/hero/ListHeroesContent.kt
@@ -6,14 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
 import com.yasserakbbach.borutoapp.domain.model.Hero
+import com.yasserakbbach.borutoapp.presentation.components.hero.HeroShimmerEffect
 import com.yasserakbbach.borutoapp.ui.theme.SMALL_PADDING
-import com.yasserakbbach.borutoapp.util.HeroSample
 
 @Composable
 fun ListHeroesContent(
@@ -21,31 +20,49 @@ fun ListHeroesContent(
     heroes: LazyPagingItems<Hero>,
     navController: NavHostController,
 ) {
-    LazyColumn(
-        modifier = modifier.fillMaxSize(),
-        contentPadding = PaddingValues(all = SMALL_PADDING),
-        verticalArrangement = Arrangement.spacedBy(SMALL_PADDING),
-    ) {
-        items(
-            items = heroes,
-            key = { hero -> hero.id },
-        ) { hero ->
-            hero?.let {
-                HeroItem(
-                    hero = it,
-                    navController = navController,
-                )
-            }
+    val results = handlePagingResult(heroes = heroes)
 
+    if(results) {
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(all = SMALL_PADDING),
+            verticalArrangement = Arrangement.spacedBy(SMALL_PADDING),
+        ) {
+            items(
+                items = heroes,
+                key = { hero -> hero.id },
+            ) { hero ->
+                hero?.let {
+                    HeroItem(
+                        hero = it,
+                        navController = navController,
+                    )
+                }
+
+            }
         }
     }
 }
 
-@Preview
 @Composable
-fun HeroItemPreview() {
-    HeroItem(
-        hero = HeroSample.heroPage1().random(),
-        navController = rememberNavController()
-    )
+fun handlePagingResult(
+    heroes: LazyPagingItems<Hero>,
+): Boolean {
+    heroes.apply {
+        val error = when {
+            loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+            loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+            loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+            else -> null
+        }
+
+        return when {
+            loadState.refresh is LoadState.Loading -> {
+                HeroShimmerEffect()
+                false
+            }
+            error != null -> false
+            else -> true
+        }
+    }
 }
